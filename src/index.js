9ad741bd7cc36caf683334dbda31cbcb1db562da
Massimo Iacolare <info@massimoiacolare.it>
2019-08-19 17:12:36 +0200

    render App component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./views/app.js"
+
 console.log("Hello World!")
+
+ReactDOM.render(<App />, document.getElementById("app-container"))
