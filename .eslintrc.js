df7729f5317fd82e26b31616111de0c8085d957b
Massimo Iacolare <info@massimoiacolare.it>
2019-08-20 11:50:55 +0200

    minor

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,6 @@ module.exports = {
         browser: true,
         es6: true,
     },
-    extends: [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:prettier/recommended",
-    ],
-    globals: {
-        Atomics: "readonly",
-        SharedArrayBuffer: "readonly",
-    },
     parserOptions: {
         ecmaFeatures: {
             jsx: true,
@@ -19,16 +10,22 @@ module.exports = {
         ecmaVersion: 2018,
         sourceType: "module",
     },
+    globals: {},
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:prettier/recommended",
+    ],
     plugins: ["react", "prettier"],
-    rules: {
-        "prettier/prettier": ["error"],
-        "react/prop-types": ["off"], // Disabled cause it's a playground. Enable it in prod
-        "no-unused-vars": ["warn"], // Disabled cause it's a playground. Enable it in prod
-    },
     settings: {
         react: {
             pragma: "React",
             version: "detect",
         },
+    },
+    rules: {
+        "prettier/prettier": ["error"],
+        "react/prop-types": ["off"], // Disabled cause it's a playground. Enable it in prod
+        "no-unused-vars": ["warn"], // Disabled cause it's a playground. Enable it in prod
     },
 }
